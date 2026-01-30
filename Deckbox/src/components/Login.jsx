bcca1d7294068104a3ae04b0eb7db75a8c8e9071
Christopher Watkins <chrisbwatkins@gmail.com>
2026-01-30 11:43:30 -0700

    inserting those styles

--- Deckbox/src/components/Login.jsx
+++ Deckbox/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import {useNavigate,} from 'react-router-dom';
+import "../styles/Login.css";
 
 // Vite exposes env vars on import.meta.env. Use VITE_API_BASE for the frontend.
 const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000';
@@ -42,16 +43,16 @@ useEffect(() => {
   }, []);
 
   return (
-    <div className="login">
+    <div className="Login">
       <h3>Login</h3>
-      <form onSubmit={handleLogin}>
-        <input placeholder="username" value={username} onChange={e=>setUsername(e.target.value)} />
-        <input placeholder="password" type="password" value={password} onChange={e=>setPassword(e.target.value)} />
-        <button type="submit">Login</button>
+      <form onSubmit={handleLogin} className='Login_Form'>
+        <input placeholder="username" value={username} onChange={e=>setUsername(e.target.value)} className='Login_Form-Input Login_Form-Username' />
+        <input placeholder="password" type="password" value={password} onChange={e=>setPassword(e.target.value)} className='Login_Form-Input Login_Form-Password' />
+        <button type="submit" className='Login_Form-Button Login_Form-Submit'>Login</button>
       </form>
       <div style={{marginTop:8}}>
-        <button onClick={()=>{ setError(null); onShowRegister && onShowRegister(); }}>Create account</button>
-        <button onClick={()=>{ setError(null); onShowForgot && onShowForgot(); }} style={{marginLeft:8}}>Forgot password</button>
+        <button onClick={()=>{ setError(null); onShowRegister && onShowRegister(); }} className='Login_Form-Button Login_Form-Create'>Create account</button>
+        <button onClick={()=>{ setError(null); onShowForgot && onShowForgot(); }} style={{marginLeft:8}} className='Login_Form-Button Login_Form-Forgot'>Forgot password</button>
       </div>
       {error && <div style={{color:'red', marginTop:8}}>{error}</div>}
     </div>
