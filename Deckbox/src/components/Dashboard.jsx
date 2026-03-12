243cb74c84d84b885902df46bceeeadb5ba89d7c
Christopher Watkins <chrisbwatkins@gmail.com>
2026-03-12 10:45:55 -0600

    learning about gradients for the bento box objects

--- Deckbox/src/components/Dashboard.jsx
+++ Deckbox/src/components/Dashboard.jsx
@@ -3,22 +3,32 @@ import Navigation from '../modules/Navigation'
 import PublicDeckDisplay from './PublicDeckDisplay'
 import '../styles/Dashboard.css';
 import { Link } from 'react-router-dom';
+import { useMemo } from 'react';
 
 // Dashboard component
-function Dashboard() {
+function Dashboard({x,y}) {
+
+ const positions = useMemo(()=>{
+    return Array.from({length:5},()=>({
+      x:Math.floor(Math.random() * 100),
+      y:Math.floor(Math.random() * 100)
+    }))
+  },[])
   
   return (
   
     <div className='dashboard'>
-      <div className='dashboard_container-header'>
+      <div style={{'--gradient-x':`${positions[0].x}%`,'--gradient-y':`${positions[0].y}%`,}} className='dashboard_container-header'>
        <h1 className='header dashboard_header'> Welcome to Deckbox Dashboard </h1>
-       <Storage />
         </div>
         <div className='dashboard_container-newDeck'>
                   <p>Start a new Deck?</p>
           <Link className='dashboard_container-sub-link' to={'/Deck/new'}>+</Link>
         </div>
-        <div className='dashboard_container-sub-lowerBlock'/>
+        <div className='dashboard_container-sub-lowerBlock'>
+         <Storage />
+
+        </div>
         <div className='dashboard_container-sub-upperBlock'/>
  
 
