50cfbb3f4adfcfb5a2481c6d0df9c809f5e0a903
Christopher Watkins <chrisbwatkins@gmail.com>
2026-04-09 11:38:03 -0600

    added some style to the login feature

--- Deckbox/src/components/Login.jsx
+++ Deckbox/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import {useNavigate,} from 'react-router-dom';
 import "../styles/Login.css";
+import Gradient from '../modules/Gradient';
 
 // Vite exposes env vars on import.meta.env. Use VITE_API_BASE for the frontend.
 const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000';
@@ -44,19 +45,19 @@ useEffect(() => {
 
   return (
     <div className="login">
-      <div className='login_container container'>
+      <Gradient className='login_container container'>
       <h3 className="headers login_header">Login</h3>
       <form className="forms login_form" onSubmit={handleLogin}>
         <input placeholder="username" value={username} onChange={e=>setUsername(e.target.value)} className='inputs login_input login_input-username' />
         <input placeholder="password" type="password" value={password} onChange={e=>setPassword(e.target.value)} className='inputs login_input login_input-password' />
         <button type="submit" className='buttons submit login_button login_button-submit'>Login</button>
       </form>
-      <div style={{marginTop:8}}>
+      <div style={{marginTop:8}} className='login_buttons'>
         <button onClick={()=>{ setError(null); onShowRegister && onShowRegister(); }} className='login_button login_button-create'>Create account</button>
         <button onClick={()=>{ setError(null); onShowForgot && onShowForgot(); }} style={{marginLeft:8}} className='login_button login_button-forgot'>Forgot password</button>
       </div>
       {error && <span className="span login_span" style={{color:'red', marginTop:8}}>{error}</span>}
+    </Gradient>
       </div>
-    </div>
   )
 }
