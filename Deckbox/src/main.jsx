da7e9f6d3f1976b612beb017cedbbd31d10f0fa1
Christopher Watkins <chrisbwatkins@gmail.com>
2026-04-27 12:40:13 -0600

    was going to use tanstack but I already built the isloading section

--- Deckbox/src/main.jsx
+++ Deckbox/src/main.jsx
@@ -1,15 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClientInstance = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
 
+
   <StrictMode>
     <BrowserRouter>
+<QueryClientProvider client={queryClientInstance}>
       <App/>
+    <ReactQueryDevtools initialIsOpen={false} position="bottom-right"/>
+  </QueryClientProvider>
     </BrowserRouter>  
   </StrictMode>,
 )
