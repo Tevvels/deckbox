b459e616c729c45513be9ef53ad3e8f28a503b48
Christopher Watkins <chrisbwatkins@gmail.com>
2026-05-01 12:17:52 -0600

    fix: more case sensetive issues

--- Deckbox/client/src/components/DeckCard.jsx
+++ Deckbox/client/src/components/DeckCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import "../styles/myDecks.css"
+import "../styles/MyDecks.css"
 
 const getColorIdentity = (char) =>{
     const colorMap = {
